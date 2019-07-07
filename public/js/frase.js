8863d54fa3f02dce1cae897a2b33cb5f8f65650c
luanfonsecap <luanfonsecap@icloud.com>
2019-07-07 17:49:19 -0300

    ajax novas frases

---
+++ public/js/frase.js
@@ -0,0 +1,12 @@
+$('#botao-frase').click(fraseAleatoria);
+
+function fraseAleatoria(){
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+}
+
+function trocaFraseAleatoria(data){
+    var frase = $('.frase');
+    var numeroAleatorio = Math.floor(Matg.random() * data.length);
+
+    frase.text(numeroAleatorio);
+}
