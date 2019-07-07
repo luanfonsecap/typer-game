64fbc490c9593d92220dcb9e0477517b79aa250c
luanfonsecap <luanfonsecap@icloud.com>
2019-07-07 15:41:20 -0300

    adicionando arqvuios restantes e refatorando código

--- js/main.js
+++ js/main.js
@@ -1,80 +1,78 @@
 var tempoInicial = $("#tempo-digitacao").text();
-var campo = $('.campo-digitacao')
+var campo = $(".campo-digitacao");
 
-$(
-    atualizaTamanhoFrase(),
-    inicializaContadores(),
-    inicializaMarcadores(),
-    inicializaCronometro(),
-    $('#botao-reiniciar').click(function(){
-        reiniciaJogo()
-    })
-)
+$(function() {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    inicializaMarcadores();
+    $("#botao-reiniciar").click(reiniciaJogo);
+});
 
 function atualizaTamanhoFrase() {
-    var frase = $('.frase').text()
-    var numPalavras = frase.split(' ').length
-    var tamanhoFrase = $('#tamanho-frase')
-    
-    tamanhoFrase.text(numPalavras)
+    var frase = $(".frase").text();
+    var numPalavras  = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+
+    tamanhoFrase.text(numPalavras);
 }
 
-function inicializaContadores() {       
-    campo.on("input", function(){
-        var conteudo = campo.val()
-        var qtdPalavras = conteudo.split(/\S+/).length -1
-        $('#contador-palavras').text(qtdPalavras)
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
 
-        var qtdCaracteres = conteudo.length
-        $('#contador-caracteres').text(qtdCaracteres)
-    })
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
 }
 
-function inicializaMarcadores() {    
-    var frase = $('.frase').text()
-    campo.on("input", function(){
-        var digitado = campo.val()
-        var comparavel = frase.substr(0, digitado.length)
+function inicializaMarcadores() {
+    var frase = $(".frase").text();
+    campo.on("input", function() {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0, digitado.length);
 
-        if (digitado == comparavel){
-            campo.addClass('borda-verde')
-            campo.removeClass('borda-vermelha')
+        if (digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
         } else {
-            campo.addClass('borda-vermelha')
-            campo.removeClass('borda-verde')
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
         }
-    })
+    });
 }
 
 function inicializaCronometro() {
-    var tempoRestante = $('#tempo-digitacao').text()
+    var tempoRestante = $("#tempo-digitacao").text();
     campo.one("focus", function() {
-        var cronometroID = setInterval(function() {
-            tempoRestante--
-            $('#tempo-digitacao').text(tempoRestante)
-            console.log(tempoRestante)
-            if(tempoRestante < 1){
-                clearInterval(cronometroID)
-                finalizaJogo()
-            }
-        }, 1000);
-    })
+    	var cronometroID = setInterval(function() {
+    		tempoRestante--;
+    		$("#tempo-digitacao").text(tempoRestante);
+    		if (tempoRestante < 1) {
+                clearInterval(cronometroID);
+                finalizaJogo();
+    		}
+    	}, 1000);
+    });
 }
 
 function finalizaJogo() {
     campo.attr("disabled", true);
-    campo.addClass("campo-desativado");    
+    campo.addClass("campo-desativado");
+    inserePlacar();
 }
 
 function reiniciaJogo() {
-    campo.attr("disabled", false)
-    campo.val('')
-    $('#contador-palavras').text(0)
-    $('#contador-caracteres').text(0)
-    $('#tempo-digitacao').text(tempoInicial)
-    inicializaCronometro()
-    campo.removeClass('campo-desativado')
-    campo.removeClass('borda-vermelha')
-    campo.removeClass('borda-verde')
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text(0);
+    $("#contador-caracteres").text(0);
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+    campo.removeClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
 }
-
