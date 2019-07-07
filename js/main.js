10498d185f3f82aee9d3371b4f6bbbadcf5dcbb6
luanfonsecap <luanfonsecap@icloud.com>
2019-07-07 11:05:43 -0300

    funcao marcadores

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+var campo = $('.campo-digitacao')
+
 $(
     atualizaTamanhoFrase(),
-    inicializaContadores()
+    inicializaContadores(),
+    inicializaMarcadores()
 )
-
-
 
 function atualizaTamanhoFrase() {
     var frase = $('.frase').text()
@@ -13,8 +14,7 @@ function atualizaTamanhoFrase() {
     tamanhoFrase.text(numPalavras)
 }
 
-function inicializaContadores() {
-    var campo = $('.campo-digitacao')        
+function inicializaContadores() {       
     campo.on("input", function(){
         var conteudo = campo.val()
         var qtdPalavras = conteudo.split(/\S+/).length -1
@@ -22,5 +22,21 @@ function inicializaContadores() {
 
         var qtdCaracteres = conteudo.length
         $('#contador-caracteres').text(qtdCaracteres)
+    })
+}
+
+function inicializaMarcadores() {    
+    var frase = $('.frase').text()
+    campo.on("input", function(){
+        var digitado = campo.val()
+        var comparavel = frase.substr(0, digitado.length)
+
+        if (digitado == comparavel){
+            campo.addClass('borda-verde')
+            campo.removeClass('borda-vermelha')
+        } else {
+            campo.addClass('borda-vermelha')
+            campo.removeClass('borda-verde')
+        }
     })
 }
